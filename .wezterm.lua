631ae2ad9a5e5b1b41521146e7e6ff3645743985
YaroslavTheOmen <yaroslav.augustus@outlook.com>
2024-09-13 17:52:38 +0300

    some trash was cleaned

--- .wezterm.lua
+++
@@ -1,8 +0,0 @@
-local wezterm = require("wezterm")
-
-local config = wezterm.config_builder()
-
-config.default_prog = { "/opt/homebrew/bin/fish", "-l" }
-config.color_scheme = "One Light (base16)"
-
-return config
