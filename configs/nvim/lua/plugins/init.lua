3282b7bc6902662d0d11346fac9c48f72fe05b9f
YaroslavTheOmen <yaroslav.augustus@outlook.com>
2024-11-10 23:46:11 +0300

    fix bugs with cmp and divide init.lua
    - fix several bugs in cmp window
    - givide init.lua into copilot.lua and cmp.lua

--- configs/nvim/lua/plugins/init.lua
+++ configs/nvim/lua/plugins/init.lua
@@ -173,116 +173,7 @@ return {
       "onsails/lspkind-nvim",
     },
     config = function()
-      -- Set completeopt
-      vim.opt.completeopt = { "menu", "menuone", "noselect" }
-
-      -- Load LuaSnip
-      local luasnip = require "luasnip"
-      require("luasnip.loaders.from_vscode").lazy_load()
-
-      -- Load lspkind for symbols
-      local lspkind = require "lspkind"
-
-      -- Setup nvim-cmp
-      local cmp = require "cmp"
-
-      -- Setup capabilities for LSP (to be used in LSP setup)
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-      -- Tab Completion Configuration (Highly Recommended)
-      local has_words_before = function()
-        if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
-          return false
-        end
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match "^%s*$" == nil
-      end
-
-      cmp.setup {
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert {
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm { select = true },
-          ["<Tab>"] = vim.schedule_wrap(function(fallback)
-            if cmp.visible() and has_words_before() then
-              cmp.select_next_item { behavior = cmp.SelectBehavior.Select }
-            else
-              fallback()
-            end
-          end),
-
-          -- dafault tab mapping
-          --["<Tab>"] = cmp.mapping(function(fallback)
-          --	if cmp.visible() then
-          --		cmp.select_next_item()
-          --	elseif luasnip.expand_or_jumpable() then
-          --		luasnip.expand_or_jump()
-          --	else
-          --		fallback()
-          --	end
-          --end, { "i", "s" }),
-
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-        },
-        sources = cmp.config.sources {
-          { name = "copilot" },
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-          { name = "buffer" },
-          { name = "path" },
-          { name = "cmdline" },
-        },
-        window = {
-          completion = cmp.config.window.bordered {
-            border = "single", -- Customize as needed ("single", "rounded", etc.)
-            -- winhighlight = "Normal:CmpPmenu,FloatBorder:CmpPmenuBorder,CursorLine:PmenuSel,Search:None",
-            -- scrollbar = false, -- Disable the scrollbar
-            col_offset = 0,
-            side_padding = 1,
-          },
-          documentation = cmp.config.window.bordered {
-            border = "single",
-            -- winhighlight = "Normal:CmpPmenuDoc,FloatBorder:CmpPmenuBorder,CursorLine:PmenuSel,Search:None",
-            -- scrollbar = false, -- Disable the scrollbar for documentation window
-          },
-        },
-        experimental = {
-          ghost_text = true,
-          native_menu = false,
-        },
-      }
-
-      -- Setup for command-line mode completions
-      cmp.setup.cmdline("/", {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          { name = "buffer" },
-        },
-      })
-
-      cmp.setup.cmdline(":", {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({
-          { name = "path" },
-        }, {
-          { name = "cmdline" },
-        }),
-      })
+      require "configs.cmp"
     end,
   },
 
@@ -292,53 +183,11 @@ return {
     cmd = "Copilot",
     event = "InsertEnter",
     config = function()
-      require("copilot").setup {
-        panel = {
-          enabled = false,
-          auto_refresh = false,
-          keymap = {
-            jump_prev = "[[",
-            jump_next = "]]",
-            accept = "<CR>",
-            refresh = "gr",
-            open = "<M-CR>",
-          },
-          layout = {
-            position = "bottom", -- | top | left | right
-            ratio = 0.4,
-          },
-        },
-        suggestion = {
-          enabled = false,
-          auto_trigger = true,
-          hide_during_completion = true,
-          debounce = 75,
-          keymap = {
-            accept = "<M-l>",
-            accept_word = false,
-            accept_line = false,
-            next = "<M-]>",
-            prev = "<M-[>",
-            dismiss = "<C-]>",
-          },
-        },
-        filetypes = {
-          yaml = false,
-          markdown = false,
-          help = false,
-          gitcommit = false,
-          gitrebase = false,
-          hgcommit = false,
-          svn = false,
-          cvs = false,
-          ["."] = false,
-        },
-        copilot_node_command = "node", -- Node.js version must be > 18.x
-        server_opts_overrides = {},
-      }
+      require "configs.copilot"
     end,
   },
 
+  -- cmp for copilot
   {
     "zbirenbaum/copilot-cmp",
     event = { "InsertEnter", "LspAttach" },
