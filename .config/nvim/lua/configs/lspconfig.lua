37db9812bc585058dcff6b84232ac03320e8d6e6
YaroslavTheOmen <yaroslav.augustus@protonmail.com>
2025-05-17 00:12:20 +0300

    updated to include blink lsp

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,355 +1,207 @@
--- Import necessary modules
-local lspconfig = require("lspconfig")
-local util = require("lspconfig/util")
-local cmp_nvim_lsp = require("cmp_nvim_lsp")
-local schemastore = require("schemastore")
+local lspconfig = require "lspconfig"
+local util = require "lspconfig.util"
+local schemastore = require "schemastore"
 
--- Initialize Mason and Mason-Lspconfig
-require("mason").setup()
-require("mason-lspconfig").setup({
-    ensure_installed = {
-        "clangd",
-        "cmake",
-        "cssls",
-        "docker_compose_language_service",
-        "dockerls",
-        "eslint",
-        "gopls",
-        "html",
-        "jsonls",
-        "lua_ls",
-        "pyright",
-        "ruff",
-        "rust_analyzer",
-        "solidity",
-        "sqls",
-        "tailwindcss",
-        "ts_ls",
-        "yamlls",
-    },
-})
+-- A. nvim-cmp (default)  ----------------------------------------------
+local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
--- Define capabilities
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
+-- B. Blink -------------------------------------------------------------
+-- local capabilities = require("blink.cmp").get_lsp_capabilities(
+--   vim.lsp.protocol.make_client_capabilities()
+-- )
+-- ======================================================================
 
--- Custom on_attach function to set keybindings and disable LSP formatting
+-- on_attach ------------------------------------------------------------
 local function custom_on_attach(client, bufnr)
-    local opts = { noremap = true, silent = true }
-    local keymap = vim.api.nvim_buf_set_keymap
+  local map, opts = vim.api.nvim_buf_set_keymap, { noremap = true, silent = true }
 
-    keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-    keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-    keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-    keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-    keymap(bufnr, "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-    keymap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-    keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-    keymap(bufnr, "n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
-    keymap(bufnr, "n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
+  map(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+  map(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+  map(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+  map(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+  map(bufnr, "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+  map(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+  map(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+  map(bufnr, "n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
+  map(bufnr, "n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
 
-    -- Disable formatting via LSP to prevent conflicts with other formatters
-    if client.server_capabilities.document_formatting then
-        client.server_capabilities.document_formatting = false
-    end
-    if client.server_capabilities.document_range_formatting then
-        client.server_capabilities.document_range_formatting = false
-    end
+  if client.server_capabilities.document_formatting then
+    client.server_capabilities.document_formatting = false
+  end
+  if client.server_capabilities.document_range_formatting then
+    client.server_capabilities.document_range_formatting = false
+  end
 end
 
--- LSP server-specific settings
+-- Per-server overrides -------------------------------------------------
 local servers = {
+  sqls = { filetypes = { "sql" }, root_dir = util.root_pattern ".git" },
 
-    sqls = {
-        filetypes = { "sql" },
-        root_dir = util.root_pattern(".git"),
-        single_file_support = true,
-        -- If you have advanced settings, you can configure them below:
-        -- settings = {
-        --   sqls = {
-        --     connections = {
-        --       {
-        --         driver = 'mysql',
-        --         dataSourceName = 'root:root@tcp(127.0.0.1:3306)/test',
-        --       },
-        --       -- More connections here...
-        --     },
-        --   },
-        -- },
-    },
+  solidity = {
+    cmd = { "solidity-language-server", "--stdio" },
+    filetypes = { "solidity" },
+    root_dir = util.root_pattern("truffle-config.js", "hardhat.config.js", ".git"),
+  },
 
-    solidity = {
-        cmd = { "solidity-language-server", "--stdio" },
-        filetypes = { "solidity" },
-        root_dir = util.root_pattern("truffle-config.js", "hardhat.config.js", ".git"),
-        settings = {
-            solidity = {
-                -- Optionally, include solidity-specific settings here.
-                -- For example, you could specify a compiler version or remappings if needed.
-            },
+  cmake = {
+    cmd = { "cmake-language-server" },
+    filetypes = { "cmake" },
+    root_dir = util.root_pattern("CMakePresets.json", "CMakeLists.txt", ".git"),
+    init_options = { buildDirectory = "build" },
+  },
+
+  lua_ls = {
+    settings = {
+      Lua = {
+        runtime = { version = "LuaJIT", path = vim.split(package.path, ";") },
+        diagnostics = { globals = { "vim" } },
+        workspace = {
+          library = vim.api.nvim_get_runtime_file("", true),
+          checkThirdParty = false,
         },
+        telemetry = { enable = false },
+      },
     },
+  },
 
-    -- cmake LSP server
-    cmake = {
-        cmd = { "cmake-language-server" },
-        filetypes = { "cmake" },
-        root_dir = util.root_pattern("CMakePresets.json", "CMakeLists.txt", ".git"),
-        init_options = {
-            -- Adjust if you have a specific build directory
-            buildDirectory = "build",
+  pyright = {
+    cmd = { "pyright-langserver", "--stdio", "--offset-encoding=utf-16" },
+    settings = {
+      python = {
+        analysis = {
+          autoImportCompletions = true,
+          useLibraryCodeForTypes = true,
+          diagnosticMode = "openFilesOnly",
         },
-        single_file_support = true,
+      },
     },
+  },
 
-    -- Lua LSP server
-    lua_ls = {
-        settings = {
-            Lua = {
-                runtime = {
-                    version = "LuaJIT",
-                    path = vim.split(package.path, ";"),
-                },
-                diagnostics = {
-                    globals = { "vim" },
-                },
-                workspace = {
-                    library = vim.api.nvim_get_runtime_file("", true),
-                    checkThirdParty = false,
-                },
-                telemetry = {
-                    enable = false,
-                },
-            },
-        },
+  gopls = {
+    filetypes = { "go", "gomod", "gowork", "gotmpl" },
+    settings = {
+      gopls = {
+        completeUnimported = true,
+        usePlaceholders = true,
+        analyses = { unusedparams = true, unreachable = true },
+      },
     },
+  },
 
-    -- Python LSP servers
-    pyright = {
-        cmd = { "pyright-langserver", "--stdio", "--offset-encoding=utf-16" },
-        settings = {
-            python = {
-                analysis = {
-                    autoImportCompletions = true,
-                    useLibraryCodeForTypes = true,
-                    diagnosticMode = "openFilesOnly",
-                },
-            },
-        },
-    },
-    ruff = {},
+  ts_ls = {},
+  tailwindcss = {},
+  eslint = {},
+  ruff = {},
 
-    -- Web development LSP servers
-    ts_ls = { -- Replaced from tsserver to ts_ls
-        settings = {
-            typescript = {
-                inlayHints = {
-                    includeInlayParameterNameHints = "all",
-                    includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-                    includeInlayFunctionParameterTypeHints = true,
-                    includeInlayVariableTypeHints = true,
-                },
-            },
-            javascript = {
-                inlayHints = {
-                    includeInlayParameterNameHints = "all",
-                    includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-                    includeInlayFunctionParameterTypeHints = true,
-                    includeInlayVariableTypeHints = true,
-                },
-            },
-        },
+  clangd = {
+    cmd = {
+      "clangd",
+      "--background-index",
+      "--clang-tidy",
+      "--completion-style=detailed",
+      "--offset-encoding=utf-16",
     },
-    tailwindcss = {},
-    eslint = {},
+    filetypes = { "c", "cpp", "objc", "objcpp" },
+    root_dir = util.root_pattern("compile_commands.json", "compile_flags.txt", ".git"),
+    capabilities = capabilities,
+  },
 
-    -- Go LSP server
-    gopls = {
-        filetypes = { "go", "gomod", "gowork", "gotmpl" },
-        settings = {
-            gopls = {
-                completeUnimported = true,
-                usePlaceholders = true,
-                analyses = {
-                    unusedparams = true,
-                    unreachable = true,
-                },
-            },
-        },
-    },
+  html = {
+    capabilities = vim.tbl_extend("keep", capabilities, { documentFormatting = false }),
+    settings = { html = { format = { enable = true }, hover = { documentation = true } } },
+  },
 
-    -- C/C++ LSP server
-    clangd = {
-        cmd = {
-            "clangd",
-            "--background-index",
-            "--clang-tidy",
-            "--completion-style=detailed",
-            "--offset-encoding=utf-16",
-        },
-        filetypes = { "c", "cpp", "objc", "objcpp" },
-        root_dir = util.root_pattern("compile_commands.json", "compile_flags.txt", ".git"),
-        capabilities = capabilities,
+  cssls = {
+    capabilities = vim.tbl_extend("keep", capabilities, { documentFormatting = false }),
+    settings = {
+      css = { validate = true, lint = { unknownAtRules = "ignore" } },
+      scss = { validate = true, lint = { unknownAtRules = "ignore" } },
+      less = { validate = true, lint = { unknownAtRules = "ignore" } },
     },
+  },
 
-    -- HTML LSP server
-    html = {
-        capabilities = vim.tbl_extend("keep", capabilities, {
-            documentFormatting = false,
-        }),
-        settings = {
-            html = {
-                format = {
-                    enable = true,
-                },
-                hover = {
-                    documentation = true,
-                    references = true,
-                },
-            },
-        },
-    },
+  jsonls = {
+    settings = { json = { schemas = schemastore.json.schemas(), validate = true } },
+  },
 
-    -- CSS LSP server
-    cssls = {
-        capabilities = vim.tbl_extend("keep", capabilities, {
-            documentFormatting = false,
-        }),
-        settings = {
-            css = {
-                validate = true,
-                lint = {
-                    unknownAtRules = "ignore",
-                },
-            },
-            scss = {
-                validate = true,
-                lint = {
-                    unknownAtRules = "ignore",
-                },
-            },
-            less = {
-                validate = true,
-                lint = {
-                    unknownAtRules = "ignore",
-                },
-            },
-        },
+  yamlls = {
+    settings = {
+      yaml = {
+        schemas = schemastore.yaml.schemas(),
+        validate = true,
+        schemaStore = { enable = false, url = "" },
+      },
     },
+  },
 
-    -- JSON LSP server with schemastore
-    jsonls = {
-        settings = {
-            json = {
-                schemas = schemastore.json.schemas(),
-                validate = { enable = true },
-            },
-        },
-    },
+  dockerls = { filetypes = { "Dockerfile" }, root_dir = util.root_pattern("Dockerfile", ".git") },
 
-    -- YAML LSP server with schemastore
-    yamlls = {
-        settings = {
-            yaml = {
-                schemas = schemastore.yaml.schemas(),
-                validate = true,
-                schemaStore = {
-                    enable = false,
-                    url = "",
-                },
-            },
-        },
-    },
-
-    -- Docker-related LSPs
-    -- Dockerfile language server
-    dockerls = {
-        filetypes = { "Dockerfile" },
-        root_dir = util.root_pattern("Dockerfile", ".git"),
-    },
-
-    -- Docker Compose language server (custom setup, since it's not standard in nvim-lspconfig)
-    docker_compose_language_service = {
-        -- Usually the npm package is: docker-compose-language-service
-        -- The binary is often named: docker-compose-langserver
-        cmd = { "docker-compose-langserver", "--stdio" },
-        filetypes = { "yaml", "yml" },
-        root_dir = util.root_pattern("docker-compose.yml", "docker-compose.yaml", ".git"),
-        settings = {},
-    },
+  docker_compose_language_service = {
+    cmd = { "docker-compose-langserver", "--stdio" },
+    filetypes = { "yaml", "yml" },
+    root_dir = util.root_pattern("docker-compose.yml", "docker-compose.yaml", ".git"),
+  },
 }
 
--- Setup LSP servers with mason-lspconfig
-require("mason-lspconfig").setup_handlers({
-    -- Handler for rust_analyzer to prevent double setup
-    ["rust_analyzer"] = function()
-        -- Do nothing; rustaceanvim will handle rust_analyzer
+-- Mason ----------------------------------------------------------------
+require("mason").setup()
+
+require("mason-lspconfig").setup {
+  ensure_installed = {
+    "clangd",
+    "cmake",
+    "cssls",
+    "docker_compose_language_service",
+    "dockerls",
+    "eslint",
+    "gopls",
+    "html",
+    "jsonls",
+    "lua_ls",
+    "pyright",
+    "ruff",
+    "rust_analyzer",
+    "solidity",
+    "sqls",
+    "tailwindcss",
+    "ts_ls",
+    "yamlls",
+  },
+
+  handlers = {
+    ["rust_analyzer"] = function() end, -- rustaceanvim manages this one
+
+    function(server)
+      local opts = { on_attach = custom_on_attach, capabilities = capabilities }
+      if servers[server] then
+        opts = vim.tbl_deep_extend("force", opts, servers[server])
+      end
+      lspconfig[server].setup(opts)
     end,
+  },
+}
 
-    -- Default handler for all other servers
-    function(server_name)
-        local config = {
-            on_attach = custom_on_attach,
-            capabilities = capabilities,
-        }
-
-        -- Apply specific settings based on the server
-        if servers[server_name] then
-            config = vim.tbl_deep_extend("force", config, servers[server_name])
-        end
-
-        lspconfig[server_name].setup(config)
-    end,
-})
-
+-- RustaceanVim ---------------------------------------------------------
 vim.g.rustaceanvim = {
-    server = {
-        on_attach = custom_on_attach,
-        capabilities = capabilities,
-        settings = {
-            ["rust-analyzer"] = {
-                -- instead of a table here, this is just on/off
-                checkOnSave = true,
-                -- move your “clippy” override into `check`
-                check = {
-                    command = "clippy",
-                },
-
-                diagnostics = {
-                    enable = true,
-                    disabled = {},
-                    enableExperimental = true,
-                },
-
-                cargo = {
-                    -- lowercase "all", or an array of feature-names
-                    features = "all",
-                    loadOutDirsFromCheck = true,
-                },
-
-                procMacro = { enable = true },
-
-                completion = {
-                    addCallArgumentSnippets = true,
-                    postfix = { enable = true },
-                },
-
-                hover = {
-                    actions = { references = true, implementation = true },
-                },
-
-                rustfmt = {
-                    enableRangeFormatting = true,
-                    extraArgs = { "--edition", "2021" },
-                },
-
-                experimental = { procAttrMacros = true },
-
-                assist = {
-                    importGranularity = "module",
-                    importPrefix = "by_self",
-                },
-            },
+  server = {
+    on_attach = custom_on_attach,
+    capabilities = capabilities,
+    settings = {
+      ["rust-analyzer"] = {
+        checkOnSave = true,
+        check = { command = "clippy" },
+        diagnostics = { enable = true, enableExperimental = true },
+        cargo = { features = "all", loadOutDirsFromCheck = true },
+        procMacro = { enable = true },
+        completion = {
+          addCallArgumentSnippets = true,
+          postfix = { enable = true },
         },
+        hover = { actions = { references = true, implementation = true } },
+        rustfmt = { enableRangeFormatting = true, extraArgs = { "--edition", "2021" } },
+        experimental = { procAttrMacros = true },
+        assist = { importGranularity = "module", importPrefix = "by_self" },
+      },
     },
+  },
 }
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,193 +1,159 @@
 return {
-    -- v3.0 plugin
-    "nvim-lua/plenary.nvim",
-    {
-        "nvchad/ui",
-        config = function()
-            require("nvchad")
-        end,
+
+  -- ► A. nvim-cmp  -----------------------------------------------------
+  {
+    "hrsh7th/nvim-cmp",
+    event = "VeryLazy",
+    dependencies = {
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "saadparwaiz1/cmp_luasnip",
+      "hrsh7th/cmp-cmdline",
+      "L3MON4D3/LuaSnip",
+      "rafamadriz/friendly-snippets",
+      "hrsh7th/cmp-nvim-lsp",
+      "onsails/lspkind-nvim",
     },
+    config = function()
+      require "configs.cmp"
+    end,
+  },
 
-    -- v3.0 plugin
-    {
-        "nvchad/base46",
-        lazy = true,
-        build = function()
-            require("base46").load_all_highlights()
-        end,
+  -- ► B. Blink  --------------------------------------------------------
+  -- { import = "nvchad.blink.lazyspec" },
+  -- { "Saghen/blink.cmp", opts = {} },
+
+  -- Core LSP / Mason stack
+  { "neovim/nvim-lspconfig" },
+  { "williamboman/mason.nvim", build = ":MasonUpdate" },
+  { "williamboman/mason-lspconfig.nvim" },
+
+  -- UI & NvChad core
+  "nvim-lua/plenary.nvim",
+  {
+    "nvchad/ui",
+    config = function()
+      require "nvchad"
+    end,
+  },
+  {
+    "nvchad/base46",
+    lazy = true,
+    build = function()
+      require("base46").load_all_highlights()
+    end,
+  },
+
+  -- Misc NvChad v3 additions
+  { "nvzone/volt", lazy = true },
+  { "nvzone/menu", lazy = true },
+  { "nvzone/minty", cmd = { "Shades", "Huefy" } },
+
+  -- Utilities
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = "LazyGit",
+    init = function()
+      vim.g.lazygit_floating_window_use_plenary = 0
+    end,
+  },
+  { "akinsho/toggleterm.nvim", version = "*", event = "VeryLazy", config = true },
+  {
+    "folke/trouble.nvim",
+    cmd = "Trouble",
+    config = function()
+      dofile(vim.g.base46_cache .. "trouble")
+      require("trouble").setup()
+    end,
+  },
+  {
+    "folke/todo-comments.nvim",
+    event = "VeryLazy",
+    dependencies = "nvim-lua/plenary.nvim",
+    opts = {},
+    config = function(_, opts)
+      require("todo-comments").setup(opts)
+    end,
+  },
+
+  -- Formatting / linting
+  {
+    "nvimtools/none-ls.nvim",
+    config = function()
+      require "configs.none-ls"
+    end,
+  },
+  {
+    "mfussenegger/nvim-lint",
+    event = "VeryLazy",
+    config = function()
+      require "configs.nvim-lint"
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre",
+    config = function()
+      require "configs.conform"
+    end,
+  },
+
+  -- Treesitter + extras
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "c",
+        "cmake",
+        "cpp",
+        "css",
+        "dockerfile",
+        "go",
+        "html",
+        "javascript",
+        "lua",
+        "python",
+        "rust",
+        "solidity",
+        "sql",
+        "tsx",
+        "typescript",
+        "vim",
+      },
+      auto_install = true,
     },
-
-    -- v3.0 plugin
-    { "nvzone/volt", lazy = true },
-
-    -- v3.0 plugin
-    { "nvzone/menu", lazy = true },
-
-    -- v3.0 plugin
-    {
-        "nvzone/minty",
-        cmd = { "Shades", "Huefy" },
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    ft = {
+      "html",
+      "xml",
+      "javascript",
+      "javascriptreact",
+      "typescript",
+      "typescriptreact",
     },
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
 
-    -- Base for NvChad
-    { "williamboman/mason.nvim" },
-    { "williamboman/mason-lspconfig.nvim" },
-    { "neovim/nvim-lspconfig" },
-    { "nvchad/showkeys", cmd = "ShowkeysToggle" },
+  -- Language-specific helpers
+  {
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    config = function(_, opts)
+      require("gopher").setup(opts)
+    end,
+    build = function()
+      vim.cmd [[silent! GoInstallDeps]]
+    end,
+  },
+  { "mrcjkb/rustaceanvim", version = "^5", ft = "rust" },
 
-    -- LazyGit
-    {
-        "kdheepak/lazygit.nvim",
-        cmd = "LazyGit",
-        config = function()
-            vim.g.lazygit_floating_window_use_plenary = 0 -- Disables floating terminal
-        end,
-    },
+  -- Git
+  { "tpope/vim-fugitive", event = "VeryLazy" },
 
-    -- toggleterm
-    { "akinsho/toggleterm.nvim", version = "*", config = true, event = "VeryLazy" },
-
-    -- v3.0
-    {
-        "folke/trouble.nvim",
-        cmd = "Trouble",
-        config = function()
-            dofile(vim.g.base46_cache .. "trouble")
-            require("trouble").setup()
-        end,
-    },
-
-    -- todo comments
-    {
-        "folke/todo-comments.nvim",
-        event = "VeryLazy",
-        requires = { "nvim-lua/plenary.nvim" },
-        opts = {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-        },
-        config = function(_, opts)
-            require("todo-comments").setup(opts)
-        end,
-    },
-
-    -- none-ls
-    {
-        "nvimtools/none-ls.nvim",
-        config = function()
-            require("configs.none-ls")
-        end,
-    },
-
-    -- nvim-lint
-    {
-        "mfussenegger/nvim-lint",
-        event = "VeryLazy",
-        config = function()
-            require("configs.nvim-lint")
-        end,
-    },
-
-    -- conform
-    {
-        "stevearc/conform.nvim",
-        event = "BufWritePre",
-        config = function()
-            require("configs.conform")
-        end,
-    },
-
-    -- nvim-ts-autotag for web development
-    {
-        "windwp/nvim-ts-autotag",
-        ft = { "html", "xml", "javascript", "javascriptreact", "typescript", "typescriptreact" },
-        config = function()
-            require("nvim-ts-autotag").setup()
-        end,
-    },
-
-    -- Gopher for Go development
-    {
-        "olexsmir/gopher.nvim",
-        ft = "go",
-        config = function(_, opts)
-            require("gopher").setup(opts)
-        end,
-        build = function()
-            vim.cmd([[silent! GoInstallDeps]])
-        end,
-    },
-
-    -- RustaceanVim for Rust development
-    {
-        "mrcjkb/rustaceanvim",
-        version = "^5",
-        lazy = false,
-        ft = { "rust" },
-        dependencies = "neovim/nvim-lspconfig",
-    },
-
-    -- nvim-lspconfig with schemastore
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = { "b0o/schemastore.nvim" },
-        config = function()
-            require("configs.lspconfig")
-        end,
-    },
-
-    -- Mason for managing external tools
-    {
-        "williamboman/mason.nvim",
-    },
-
-    -- Treesitter for syntax highlighting
-
-    {
-        "nvim-treesitter/nvim-treesitter",
-        opts = {
-            ensure_installed = {
-                "c",
-                "cmake",
-                "cpp",
-                "css",
-                "dockerfile",
-                "go",
-                "html",
-                "javascript",
-                "lua",
-                "python",
-                "rust",
-                "solidity",
-                "sql",
-                "tsx",
-                "typescript",
-                "vim",
-            },
-            auto_install = true,
-        },
-    },
-
-    -- Git integration
-    { "tpope/vim-fugitive", event = "VeryLazy" },
-
-    -- nvim-cmp
-    {
-        "hrsh7th/nvim-cmp",
-        event = "VeryLazy",
-        dependencies = {
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-path",
-            "saadparwaiz1/cmp_luasnip",
-            "hrsh7th/cmp-cmdline",
-            "L3MON4D3/LuaSnip",
-            "rafamadriz/friendly-snippets",
-            "hrsh7th/cmp-nvim-lsp",
-            "onsails/lspkind-nvim",
-        },
-        config = function()
-            require("configs.cmp")
-        end,
-    },
+  -- JSON / YAML schemas
+  { "b0o/schemastore.nvim", lazy = true },
 }
