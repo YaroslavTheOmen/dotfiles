6b1674b44426be60e5001d590bdbae015a1b09e3
YaroslavTheOmen <yaroslav.augustus@protonmail.com>
2025-09-28 19:02:13 +0300

    add miniconda

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,4 +13,17 @@ end
 
 set -Ux EDITOR nvim
 
-set -x PATH /usr/local/sbin /usr/local/bin /System/Cryptexes/App/usr/bin /usr/bin /bin /usr/sbin /sbin /var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin /var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin /var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin /opt/homebrew/bin /usr/bin /bin /usr/sbin /sbin /usr/local/bin /opt/homebrew/opt/python@3.13/bin /Users/yaroslavaugustus/.foundry/bin ~/.config/emacs/bin ~/go/bin ~/.local/bin /opt/homebrew/opt/llvm/
+set -x PATH /usr/local/sbin /usr/local/bin /System/Cryptexes/App/usr/bin /usr/bin /bin /usr/sbin /sbin /var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin /var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin /var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin /opt/homebrew/bin /usr/bin /bin /usr/sbin /sbin /usr/local/bin /opt/homebrew/opt/python@3.13/bin /Users/yaroslavaugustus/.foundry/bin ~/.config/emacs/bin ~/go/bin ~/.local/bin /opt/homebrew/opt/llvm/ ~/miniconda3/bin
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /Users/yaroslavaugustus/miniconda3/bin/conda
+    eval /Users/yaroslavaugustus/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/Users/yaroslavaugustus/miniconda3/etc/fish/conf.d/conda.fish"
+        . "/Users/yaroslavaugustus/miniconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/Users/yaroslavaugustus/miniconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
