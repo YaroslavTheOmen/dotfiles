e0c460a3d17af78aaee02844323a78c64577e8d1
YaroslavTheOmen <yaroslav.augustus@protonmail.com>
2025-11-09 21:04:08 +0300

    add tree-sitter fo js in doom

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -146,7 +146,7 @@
        (json +lsp)         ; At least it ain't XML
        ;;janet             ; Fun fact: Janet is me!
        ;;(java +lsp)       ; the poster child for carpal tunnel syndrome
-       (javascript +lsp)   ; all(hope(abandon(ye(who(enter(here))))))
+       (javascript +lsp +tree-sitter)   ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
        ;;latex             ; writing papers in Emacs has never been so fun
