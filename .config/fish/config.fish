a02c06d94f5be1a00be41be24c0fa9129fe4a00a
YaroslavTheOmen <yaroslav.augustus@protonmail.com>
2025-11-22 17:02:52 +0300

    update fish config for ubuntu

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -34,22 +34,22 @@ end
 # if status is-interactive
 #     # Commands to run in interactive sessions can go here
 # end
-#
+
 # /usr/local/bin/starship init fish | source
-#
+
 # function fish_greeting
 #     echo "Current Date and Time: "(date "+%Y-%m-%d %H:%M:%S")
 #     echo "Kernel: "(uname -s)" "(uname -r)
 # end
-#
+
 # function b
 #     bash -c "$argv"
 # end
-#
+
 # set -Ux EDITOR nvim
-#
-# set -x PATH ~/.local/bin ~/.cargo/bin ~/go/bin ~/.npm-global/bin ~/.local/share/npm/bin ~/.config/yarn/global/node_modules/.bin ~/.config/emacs/bin ~/.pyenv/shims ~/.pyenv/bin /usr/local/go/bin /home/yaroslavaugustus/miniconda3/condabin /usr/local/bin /usr/local/sbin /usr/bin /usr/sbin /bin /sbin /snap/bin /usr/games /usr/local/games ~/elixir-ls/release/
-#
+
+# set -x PATH ~/.local/bin ~/.cargo/bin ~/go/bin ~/.npm-global/bin ~/.local/share/npm/bin ~/.config/yarn/global/node_modules/.bin ~/.config/emacs/bin ~/.pyenv/shims ~/.pyenv/bin /usr/local/go/bin /home/yaroslavaugustus/miniconda3/condabin /usr/local/bin /usr/local/sbin /usr/bin /usr/sbin /bin /sbin /snap/bin /usr/games /usr/local/games ~/elixir-ls/release/ /home/yaroslavaugustus/.foundry/bin ~/.bun/bin ~/.nvm
+
 # # >>> conda initialize >>>
 # # !! Contents within this block are managed by 'conda init' !!
 # if test -f "$HOME/miniconda3/bin/conda"
@@ -60,6 +60,7 @@ end
 #     set -gx PATH "$HOME/miniconda3/bin" $PATH
 # end
 # # <<< conda initialize <<<
-#
+
 # bass source /opt/ros/jazzy/setup.bash
+# source ~/.cargo/env.fish
 # source ~/.asdf/asdf.fish
